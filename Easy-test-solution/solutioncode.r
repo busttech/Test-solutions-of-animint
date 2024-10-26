1140dfc4505802658c88dd787624dd0260d44f5f
Tarun Choudhary <tarun1940c@gmail.com>
2024-10-26 13:12:55 +0530

    Update solutioncode.r

--- Easy-test-solution/solutioncode.r
+++ Easy-test-solution/solutioncode.r
@@ -1,1 +1,18 @@
-
+library(animint2)
+WorldBank1975 <- subset(WorldBank, year==1975)
+income.colors <- 
+  c("#422680", "#341671", "#280659", "#660f56", "#ae2d68", "#f54952")
+names(region.colors) <- levels(WorldBank$region)
+scatter <- ggplot()+
+  scale_color_manual(values=income.colors)+
+  geom_point(
+    mapping=aes(x=life.expectancy, y=fertility.rate, color=income),
+    data=WorldBank1975)
+two.layers <- scatter+
+  geom_path(aes(
+    x=life.expectancy, 
+    y=fertility.rate, 
+    color=income,
+    group=country),
+    data=WorldBankBefore1975)
+(viz.two.layers <- animint(two.layers,out.dir="ghgh"))
